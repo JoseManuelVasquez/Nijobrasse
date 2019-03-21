180f9d354980d380af96c052bfacc139105fc234
Jose Vasquez <jose.vasquez@avanade.com>
2019-03-21 16:31:29 +0100

    reactJS integration

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
 
 var app = express();
@@ -14,9 +13,8 @@ app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
 app.use('/user', usersRouter);
 
 module.exports = app;
