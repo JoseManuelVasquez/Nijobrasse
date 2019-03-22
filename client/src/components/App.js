9d31dc53fcfae626a8fd6dd36c7f6d8e3f797019
Jose Vasquez <jose.vasquez@avanade.com>
2019-03-22 12:46:55 +0100

    reactjs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import '../styles/App.css';
+
+const Button = styled.button`
+  background: #1EB980;
+  border-radius: 8px;
+  color: white;
+  height: ${props => props.small ? 40 : 60}px;
+  width: ${props => props.small ? 60 : 120}px;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Welcome
+            Welcome to Nijobrasse
+            <Button large>Sign Up</Button>
         </header>
       </div>
     );
